9f2fff0b17a5ed7f57a60c6624a34cf661f6bdb0
Evance <uriosoft@topcomprinters.com>
2018-01-29 18:26:52 +0300

    urio test commit

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Library IS</title>
+	<title>UrioEdit Library IS</title>
 	<link rel="stylesheet" href="css/authpages.css">
 </head>
 <body>
